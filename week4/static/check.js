7d8fd44089ff457efb819f41b0ce55853c42e58d
Angie <59827584+Angiemou06@users.noreply.github.com>
2023-07-28 13:34:46 +0800

    Add files via upload

--- week4/static/check.js
+++ week4/static/check.js
@@ -1,26 +1,23 @@
-
-document.getElementById("checkButton").addEventListener("click", check)
-
 function check() {
     let confirm = document.querySelector("#confirm");
     if (confirm.checked == false) {
         alert("Please check the checkbox first.");
+        return false
     }
 }
 
-document.getElementById("calButton").addEventListener("click", checkNumber)
 
 function checkNumber() {
     
     let number = document.getElementById("number").value;
-    number = parseInt(number);
     if (isNaN(number) || number <= 0) {
         alert("Please enter a valid positive number.");
-        window.location.href = `/`;
+        return false;
     }
     else{
+        console.log(number);
         number = number.toString();
         window.location.href = `/square/${number}`;
-
+        return false;
     }
 }
